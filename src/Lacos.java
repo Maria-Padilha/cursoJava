131f14a6e4a7a59511325f2a1d17491f960ddee0
maria-padilha <padilhabarbosa23@gmail.com>
2023-12-19 09:27:19 -0400

    commit for

--- src/Lacos.java
+++ src/Lacos.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Lacos {
     public static void main(String[] args) {
-        
+        System.out.println("Laços de repetição");
+        System.out.println(" - Laço For");
+
+        System.out.println("");
+        System.out.println("TABUADA");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Escreva até qual valor você quer: ");
+        int qtdTabuada = scan.nextInt();
+
+        System.out.printf("Agora escreva qual tabuada você quer: ");
+        int qualTabuada = scan.nextInt();
+
+        for(int tabuada = 1; tabuada <= qtdTabuada; tabuada++){
+            int mult = tabuada * qualTabuada;
+            System.out.println(qualTabuada + " x " + tabuada + " = " +mult);
+        }
     }
 }
