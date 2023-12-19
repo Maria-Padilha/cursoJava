c2d0ac52ff107328eca530c83f81cb75ee0023f5
maria-padilha <padilhabarbosa23@gmail.com>
2023-12-19 12:56:38 -0400

    testing agency code

--- src/contas/Conta.java
+++ src/contas/Conta.java
@@ -4,6 +4,8 @@ public class Conta {
     // atributos
     protected String cliente;
     protected double saldo;
+    protected double extrato;
+    protected String action;
 
     // construtor
     public Conta(){
@@ -13,5 +15,13 @@ public class Conta {
     // métodos
     protected void exibirSaldo(){
         System.out.println("Saldo: R$ "+saldo);
+    }
+
+    protected void exibirExtrato(){
+        System.out.println("Saldo: R$ "+extrato);
+    }
+
+    protected void exibirExtratoDetalhado(){
+        System.out.println("Saldo: R$ "+extrato);
     }
 }
--- src/contas/PessoaFisica.java
+++ src/contas/PessoaFisica.java
@@ -4,16 +4,51 @@ import java.util.Scanner;
 
 public class PessoaFisica {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Conta cc = new Conta();
 
+        Scanner scan = new Scanner(System.in);  
+
+        Conta cc = new Conta();
+        
         System.out.printf("Pessoa Física, digite o seu nome: ");
         cc.cliente = scan.nextLine();
 
-        System.out.printf("Pessoa física, digite o seu saldo: ");
+        System.out.printf("Deseja realizar alguma ação? (s/n): ");
+        cc.action = scan.nextLine();
+
+        System.out.printf("Pessoa física, digite o seu saldo inicial: ");
         cc.saldo = scan.nextDouble();
         
         System.out.println("Cliente: "+cc.cliente);
-        cc.exibirSaldo();
+
+        if(cc.action.equals("s")){
+            System.out.println("Digite a ação desejada: ");
+            System.out.println("1 - Depósito / 2 - Saque / 3 - Extrato");
+            int option = scan.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.printf("Digite o valor: ");
+                    int dep = scan.nextInt();
+                    cc.extrato = cc.saldo + dep;
+                    System.out.println("Depósito efetuado com sucesso");
+                    cc.exibirExtrato();
+                    break;
+
+                case 2:
+                    System.out.printf("Digite o valor: ");
+                    int saq = scan.nextInt();
+                    cc.extrato = cc.saldo - saq;
+                    System.out.println("Saque efetuado com sucesso");
+                    cc.exibirExtrato();
+                    break;
+
+                case 3:
+                    cc.exibirExtratoDetalhado();
+                    break;
+            
+                default:
+                    break;
+            }
+        }
     }
 }
